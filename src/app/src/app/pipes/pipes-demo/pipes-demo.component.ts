c1bb24dc4aa398c5aa5fa2fb5bf66cc472776384
Jan Joosse <j.joosse@rijkzwaan.nl>
2022-04-14 07:52:31 +0200

    Pipes

--- src/app/src/app/pipes/pipes-demo/pipes-demo.component.ts
+++ src/app/src/app/pipes/pipes-demo/pipes-demo.component.ts
@@ -1,3 +1,4 @@
+import { PercentPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pipes-demo.component.css']
 })
 export class PipesDemoComponent implements OnInit {
+  myVariable = 0.12313121212;
+  myVariable2 = 'just some text';
+  myVariable3?: string | null;
 
-  constructor() { }
+  constructor(private percentPipe: PercentPipe) { }
 
   ngOnInit(): void {
+    this.myVariable3 = this.percentPipe.transform(this.myVariable);
   }
 
 }
