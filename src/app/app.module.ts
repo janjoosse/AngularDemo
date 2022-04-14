206ded8ac717dcb0ee6ebac099c159862f6a4f81
Jan Joosse <j.joosse@rijkzwaan.nl>
2022-04-14 07:36:00 +0200

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesDemoComponent } from './src/app/pipes/pipes-demo/pipes-demo.component';
+import { TestingDemoComponent } from './src/app/testing/testing-demo/testing-demo.component';
+import { DirectivesDemoComponent } from './src/app/directives/directives-demo/directives-demo.component';
+import { ObservablesDemoComponent } from './src/app/observables/observables-demo/observables-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesDemoComponent,
+    TestingDemoComponent,
+    DirectivesDemoComponent,
+    ObservablesDemoComponent
   ],
   imports: [
     BrowserModule,
